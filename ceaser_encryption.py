9f051c142d563a0e97a0d5726f7800318490ab16
Naresh Kumar <nareshkr.22@gmail.com>
2017-08-28 10:17:34 +0530

    Crypto Program

---
+++ ceaser_encryption.py
@@ -0,0 +1,18 @@
+# -*- coding: cp1252 -*-
+alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+msg = raw_input("Enter the msg ")
+key = raw_input("Enter the key ")
+key = int(key)
+msg = msg.lower()
+encrypt=""
+for i in msg:
+        if i != ' ':
+            v = alpha.index(i)
+            d = v + key
+            a  = d%26
+            encrypt = encrypt + alpha[a]
+        else:
+            encrypt = encrypt + i
+
+print "Encrypted Message :" + encrypt
